d76cce1bb3ada5b929125392b01a31b4c60be7b2
khushboo kumari <ksingh200601@gmail.com>
2025-08-27 15:49:59 +0530

    updated

--- app/meals/[mealSlug]/page.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default async function MealDetailspage({params}) {
-    const {slug} =await params
-  return (
-    <div>slug:{params.slug}</div>
-  )
-}
--- app/page.js
+++ app/page.js
@@ -1,49 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
-import logoArea from "./assets/logoImg.jpg"
-import styles from "./page.module.css"
-import ImageSlideshow from "./components/image-slideshow/image-slideshow";
+import React from 'react'
 
-export default function Home() {
+const page = () => {
   return (
-  <>
-     <header className={styles.header}>
-        <div className={styles.logoArea}>
-        <ImageSlideshow/>
-        </div>
-        <h1 className={styles.title}>Welcome to Next Level Foods</h1>
-        <p className={styles.subtitle}>
-          Discover mouth-watering meals and connect with food lovers worldwide.
-        </p>
-        <div className={styles.headerButtons}>
-          <Link href="/community" className={styles.btnPrimary}>
-            Join The Community
-          </Link>
-          <Link href="/meals" className={styles.btnSecondary}>
-            Explore the Meals
-          </Link>
-        </div>
-      </header>
-
-      <main className={styles.main}>
-        <section className={styles.section}>
-          <h2>Next Level Foods</h2>
-          <p>
-            At Next Level Foods, we believe great meals start with fresh
-            ingredients, creative recipes, and a community that shares the same
-            passion for cooking.
-          </p>
-        </section>
-        <section className={styles.section}>
-          <h2>Why Join Us?</h2>
-          <ul>
-            <li>🍽 Access exclusive recipes</li>
-            <li>👩‍🍳 Learn from top chefs</li>
-            <li>🌎 Connect with food lovers around the globe</li>
-          </ul>
-        </section>
-      </main>
-   
-  </>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
