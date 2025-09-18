7574c8b70336c513cbcd599970e8ec9f8a4d9a0f
khushboo kumari <ksingh200601@gmail.com>
2025-09-18 16:55:48 +0530

    updated todoContoller

--- app/controllers/todoController.ts
+++ app/controllers/todoController.ts
@@ -1,5 +1,6 @@
 import Todo, {type  ITodo } from "../models/todo";
 import { connectDB } from "../lib/db";
+import { NextResponse } from "next/server";
 
 // Get all todos
 export async function getTodos() {
@@ -12,4 +13,28 @@ export async function addTodo(data: Partial<ITodo>) {
   await connectDB();
   const todo = new Todo(data);
   return await todo.save();
+}
+
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = params;
+    const { task, taskDesc, date, time } = await request.json();
+
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      id,
+      { task, taskDesc, date, time },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedTodo) {
+      return NextResponse.json({ error: "Todo not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(updatedTodo, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to update todo" }, { status: 500 });
+  }
 }
